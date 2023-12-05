c9659867ba733ce12c29773dc0c44ccff61c5b67
zraycd <vainiusvjg@gmail.com>
2023-12-05 19:09:44 +0200

    fix iframe video playing after closing section

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const iframe = document.createElement('iframe')
 const text = document.createElement('div')
 
-const link1 = document.querySelector('#link1')
-const link2 = document.querySelector('#link2')
-const link3 = document.querySelector('#link3')
-const link4 = document.querySelector('#link4')
+const popup1 = document.querySelector('#popup1 div')
+const popup2 = document.querySelector('#popup2 div')
+const popup3 = document.querySelector('#popup3 div')
+const popup4 = document.querySelector('#popup4 div')
 
-link1.addEventListener('click', () => {
-    const popup1 = document.querySelector('#popup1 div')
+document.querySelector('#link1').addEventListener('click', () => {
     popup1.appendChild(iframe)
     iframe.src = "https://www.youtube.com/embed/DqB95s47f1w?si=zMqAyDszoToOjVdy"
     popup1.appendChild(text)
@@ -16,24 +15,21 @@ link1.addEventListener('click', () => {
 
     console.log('clicked')
 });
-link2.addEventListener('click', () => {
-    const popup2 = document.querySelector('#popup2 div')
+document.querySelector('#link2').addEventListener('click', () => {
     popup2.appendChild(iframe)
     iframe.src = "https://www.youtube.com/embed/OaFk8XkgZik?si=AIO-JdGYaGeUucnP"
     popup2.appendChild(text)
     text.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus ratione quisquam dignissimos. Tempora itaque in deleniti, quos iusto officia ipsa maiores modi corrupti, dolorum tenetur obcaecati sunt eaque omnis veniam!"
     text.classList.add('popupText')
 });
-link3.addEventListener('click', () => {
-    const popup3 = document.querySelector('#popup3 div')
+document.querySelector('#link3').addEventListener('click', () => {
     popup3.appendChild(iframe)
     iframe.src = "https://www.youtube.com/embed/DF20qyZVpss?si=oxSD6Ldd2dtyeAcn"
     popup3.appendChild(text)
     text.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus ratione quisquam dignissimos. Tempora itaque in deleniti, quos iusto officia ipsa maiores modi corrupti, dolorum tenetur obcaecati sunt eaque omnis veniam!"
     text.classList.add('popupText')
 });
-link4.addEventListener('click', () => {
-    const popup4 = document.querySelector('#popup4 div')
+document.querySelector('#link4').addEventListener('click', () => {
     popup4.appendChild(iframe)
     iframe.src = "https://www.youtube.com/embed/163sJtJecZI?si=i3eB5gvmqtkXNZ0W"
     popup4.appendChild(text)
@@ -41,5 +37,15 @@ link4.addEventListener('click', () => {
     text.classList.add('popupText')
 });
 
-
-
+document.querySelector('#popup1').addEventListener('click', () => {
+    popup1.removeChild(iframe)
+});
+document.querySelector('#popup2').addEventListener('click', () => {
+    popup2.removeChild(iframe)
+});
+document.querySelector('#popup3').addEventListener('click', () => {
+    popup3.removeChild(iframe)
+});
+document.querySelector('#popup4').addEventListener('click', () => {
+    popup4.removeChild(iframe)
+});
